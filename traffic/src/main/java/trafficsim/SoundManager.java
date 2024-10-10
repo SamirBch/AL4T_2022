6d76b49d98f9417d2069c662fb8f60d797280155
SamirBch <125374091+SamirBch@users.noreply.github.com>
2024-10-10 18:54:28 +0200

    Correction SoundManager Ok

--- traffic/src/main/java/trafficsim/SoundManager.java
+++ traffic/src/main/java/trafficsim/SoundManager.java
@@ -3,24 +3,31 @@ package trafficsim;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
+import java.util.Objects;
 
-public class SoundManager{
+public class SoundManager {
 	Clip clip;
 	Clip driftClip;
-	public SoundManager(){
-	 try{
-	        AudioInputStream inputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream("/Traffic Sounds - Free Sound Effects - Traffic Sound Clips - Sound Bites.wav"));
-	        AudioInputStream driftStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream("/drift.wav"));
-	        clip = AudioSystem.getClip();
-	        driftClip = AudioSystem.getClip();
-	        clip.open(inputStream);
-	        driftClip.open(driftStream);
-	        driftClip.start();
-	        clip.loop(Clip.LOOP_CONTINUOUSLY);
-	      Thread.sleep(100000000); // looping as long as this thread is alive
-	}catch(Exception e){
-	System.out.print("Hello world");
+
+	public SoundManager() {
+		try {
+
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getResourceAsStream("/Traffic Sounds - Free Sound Effects - Traffic Sound Clips - Sound Bites.wav")));
+			AudioInputStream driftStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getResourceAsStream("/drift.wav")));
+
+			clip = AudioSystem.getClip();
+			driftClip = AudioSystem.getClip();
+
+			clip.open(inputStream);
+			driftClip.open(driftStream);
+
+			driftClip.start();
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+			Thread.sleep(100000000);
+		} catch (Exception e) {
+			System.out.println("Erreur lors du chargement du son: " + e.getMessage());
+		}
 	}
 }
-}
+
